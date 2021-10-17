df4c744f62d4c525f072c77502e8af8c96f89d7f
Archange75019 <arnaud.escalier@gmail.com>
2021-10-17 16:09:10 +0200

    base

---
+++ index.js
@@ -0,0 +1,61 @@
+function calcul(prixTotal, argentClient){
+    var euros = {
+        c1: 0.01,
+        c2: 0.02,
+        c5: 0.05,
+        c10: 0.1,
+        c20: 0.2,
+        c50: 0.5,
+        e1: 1,
+        e2: 2,
+        b5: 5,
+        b10: 10,
+        b20: 20,
+        b50: 50,
+        b100: 100,
+        b200: 200
+    }
+    var caisse = {
+        c1: 10,
+        c2: 2,
+        c5: 20,
+        c10: 40,
+        c20: 50,
+        c50: 88,
+        e1: 20,
+        e2: 40,
+        b5: 20,
+        b10: 30,
+        b20: 10,
+        b50: 10,
+        b100: 5,
+        b200: 2
+    }
+    var diff = argentClient - prixTotal;
+
+    for( items in euros){
+        if(euros[items] < diff ){
+            console.log(items)
+        }
+    }
+   
+
+    var rendu = {
+        c1: 0,
+        c2: 0,
+        c5: 0,
+        c10: 0,
+        c20: 0,
+        c50: 0,
+        e1: 0,
+        e2: 0,
+        b5: 0,
+        b10: 0,
+        b20: 0,
+        b50: 0,
+        b100: 0,
+        b200: 0
+    }
+    return  rendu
+}
+calcul(57.32, 100)
